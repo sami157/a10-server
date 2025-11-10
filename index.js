04680d7fe0f6334be675cc7898bc079a5e9e7ee9
sami157 <sami.eee157@gmail.com>
2025-11-10 22:48:45 +0600

    partner request api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const mongodbUri = process.env.MONGODB_URI
 
 const PORT = process.env.PORT || 3000;
@@ -64,6 +64,51 @@ app.get('/study-partners', async (req, res) => {
   }
 });
 
+app.post('/partner-requests', async (req, res) => {
+  const { senderEmail, receiverId } = req.body; // Sender's email and receiver's ObjectId
+
+  if (!senderEmail || !receiverId) {
+    return res.status(400).json({ message: "Sender email and receiver ID are required" });
+  }
+
+  try {
+    const db = client.db("studyMateDB");
+    const partnersCollection = db.collection("studyPartners");
+
+    const receiver = await partnersCollection.findOne({ _id: new ObjectId(receiverId) });
+
+    await partnersCollection.updateOne(
+      { _id: new ObjectId(receiverId) },
+      { $inc: { partnerCount: 1 } }
+    );
+
+    const partnerRequest = {
+      senderEmail,
+      receiverProfile: {
+        name: receiver.name,
+        email: receiver.email,
+        subject: receiver.subject,
+        studyMode: receiver.studyMode,
+        availabilityTime: receiver.availabilityTime,
+        location: receiver.location,
+        experienceLevel: receiver.experienceLevel
+      },
+      status: 'pending', // Set initial status to 'pending'
+      createdAt: new Date()
+    };
+
+    const partnerRequestsCollection = db.collection("partnerRequests");
+    const result = await partnerRequestsCollection.insertOne(partnerRequest);
+    res.status(201).json({
+      message: "Partner request sent successfully",
+      requestId: result.insertedId
+    });
+
+  } catch (err) {
+    console.log("Error sending partner request:", err);
+    res.status(500).json({ message: "Error sending partner request", error: err });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
